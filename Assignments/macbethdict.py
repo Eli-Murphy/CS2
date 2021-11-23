23956a7dce8f40c8b51ce22b070184155e9cb31e
ILikeBrunch <75328211+ILikeBrunch@users.noreply.github.com>
2021-11-23 11:18:15 -0500

    Update macbethdict.py

--- Assignments/macbethdict.py
+++ Assignments/macbethdict.py
@@ -1,4 +1,6 @@
 import string
+from operator import itemgetter
+import os
 
 def main():
     print("Hello World!")
@@ -14,9 +16,29 @@ def main():
                 counts[instance] = counts[instance] + 1
             else:
                 counts[instance] = 1
-    finaldict = dict( sorted(counts.values(), key=lambda x: x[0].lower()) )
-    for key in list(finaldict.keys()):
-        print(key, " ", finaldict[key])
+    finaldict = dict(reversed(sorted(counts.items(), key=lambda x:x[1])))
+    #finaldict = dict(sorted())
+    if os.path.exists(r"c:\Users\emurphy24\Documents\GitHub\CS2\Assignments\macbethoutput.txt"):
+        os.remove(r"c:\Users\emurphy24\Documents\GitHub\CS2\Assignments\macbethoutput.txt")
+        output = open(r"c:\Users\emurphy24\Documents\GitHub\CS2\Assignments\macbethoutput.txt", "w+")
+    else:
+        output = open(r"c:\Users\emurphy24\Documents\GitHub\CS2\Assignments\macbethoutput.txt", "w+")
+    top10ornah = input("top 10 or nah (y/n): ")
+    if top10ornah == "n":
+        for key in list(finaldict.keys()):
+            print(key, ":", finaldict[key])
+            result = str(key) + " : " + str(finaldict[key])
+            output.write(result)
+            output.write("\n")       
+    elif top10ornah == "y":
+        result = top10(finaldict)
+
+
+
+def top10(finaldict):
+    result = dict(sorted(finaldict.items(), key = itemgetter(1), reverse = True)[:10])
+
+    
 
 if __name__ == '__main__':
     main()
