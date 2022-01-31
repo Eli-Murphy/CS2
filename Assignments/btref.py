799bb381bc48956a81be0b8ca2374bb59814bd78
ILikeBrunch <75328211+ILikeBrunch@users.noreply.github.com>
2022-01-31 07:49:22 -0500

    Update btref.py

--- Assignments/btref.py
+++ Assignments/btref.py
@@ -1,202 +1,157 @@
-from random import randint
-import os
+### This is part of Codecademy coding challenge
+### Battleship with Python3 (single player version)
+import random
+## function to create boards for player
+def create_board_computer():
+    board = {
+        '1A':' ', '2A':' ', '3A':' ', '4A':' ', '5A':' ',
+        '1B':' ', '2B':' ', '3B':' ', '4B':' ', '5B':' ',
+        '1C':' ', '2C':' ', '3C':' ', '4C':' ', '5C':' ',
+        '1D':' ', '2D':' ', '3D':' ', '4D':' ', '5D':' ',
+        '1E':' ', '2E':' ', '3E':' ', '4E':' ', '5E':' ',
+        }
+    ship_locs = random.sample(board.keys(), 6)
+    for loc in ship_locs:
+        board[loc] = 'O'
+    return board
 
-#Ship Class
-class Ship:
-  def __init__(self, size, orientation, location):
-    self.size = size
+def create_board_player():
+    board = {
+        '1A':' ', '2A':' ', '3A':' ', '4A':' ', '5A':' ',
+        '1B':' ', '2B':' ', '3B':' ', '4B':' ', '5B':' ',
+        '1C':' ', '2C':' ', '3C':' ', '4C':' ', '5C':' ',
+        '1D':' ', '2D':' ', '3D':' ', '4D':' ', '5D':' ',
+        '1E':' ', '2E':' ', '3E':' ', '4E':' ', '5E':' ',
+        }
+    ships_locs = []
+    print('Hey Player! Where do you wanna place your ships (enter 6 locations)')
+    while len(ships_locs) < 6:
+        loc = input()
+        if loc not in board.keys():
+            print('That\'s out of range!')
+        if loc in ships_locs:
+            print('The slot is occupied, dude!')
+        else: 
+            ships_locs.append(loc)
+    for loc in ships_locs:
+        board[loc] = 'O'
+    return board
+
+## function to display board in a readeable format
+def display_board(board):
+    print('----------------------')
+    print('| ' + board['1A'] + ' | ' + board['2A'] + ' | ' + board['3A'] + ' | ' + board['4A'] + ' | ' + board['5A'] + ' |')
+    print('----------------------')
+    print('| ' + board['1B'] + ' | ' + board['2B'] + ' | ' + board['3B'] + ' | ' + board['4B'] + ' | ' + board['5B'] + ' |')
+    print('----------------------')
+    print('| ' + board['1C'] + ' | ' + board['2C'] + ' | ' + board['3C'] + ' | ' + board['4C'] + ' | ' + board['5C'] + ' |')
+    print('----------------------')
+    print('| ' + board['1D'] + ' | ' + board['2D'] + ' | ' + board['3D'] + ' | ' + board['4D'] + ' | ' + board['5D'] + ' |')
+    print('----------------------')
+    print('| ' + board['1E'] + ' | ' + board['2E'] + ' | ' + board['3E'] + ' | ' + board['4E'] + ' | ' + board['5E'] + ' |')
+    print('----------------------')
     
-    if orientation == 'horizontal' or orientation == 'vertical':
-      self.orientation = orientation
-    else:
-      raise ValueError("Value must be 'horizontal' or 'vertical'.")
+
+## function to play Battleship
+def game():
+    ## create boards for NPC and player
+    pc_board = create_board_computer()
+    player_board = create_board_player()
     
-    if orientation == 'horizontal':
-      if location['row'] in range(row_size):
-        self.coordinates = []
-        for index in range(size):
-          if location['col'] + index in range(col_size):
-            self.coordinates.append({'row': location['row'], 'col': location['col'] + index})
-          else:
-            raise IndexError("Column is out of range.")
-      else:
-        raise IndexError("Row is out of range.")
-    elif orientation == 'vertical':
-      if location['col'] in range(col_size):
-        self.coordinates = []
-        for index in range(size):
-          if location['row'] + index in range(row_size):
-            self.coordinates.append({'row': location['row'] + index, 'col': location['col']})
-          else:
-            raise IndexError("Row is out of range.")
-      else:
-        raise IndexError("Column is out of range.")
+    ## display player's board
+    display_board(player_board)
+    
+    ## variables to control when the game ends
+    turn = 'Player'
+    pc_point = 6
+    player_point = 6
+    round = 0
+    
+    ## lists to keep track of player and NPC's moves
+    pc_moves = []
+    player_moves = []
+    
+    for i in range(25):
+        if turn == 'Player':
+            print('----------------------')
+            print('\033[1mPlayer turn:\033[0m')
+            move = input()
+            if move not in player_board.keys():
+                print('You hit a homerun! It\'s just not a good thing in this game.')
+            elif move in player_moves:
+                print('C\'mon! You hit it twice! That is cheating.')
+            else: 
+                if pc_board[move] == 'O':
+                    pc_point -= 1 
+                    print(f'Nice shot, Player. {pc_point} to go!')
+                else:
+                    print('Oh! That was a miss. Try harder next time.')
+            
+            player_moves.append(move)
+        
+        if turn == 'Computer':
+            print('----------------------')
+            print('\033[1mComputer turn:\033[0m')
+            move = random.sample(player_board.keys(), 1)
+            print(move[0])
+            if player_board[move[0]] == 'O':
+                player_point -= 1
+                print(f'Nice shot, Computer. {player_point} to go!')
+            elif move in pc_moves:
+                print('C\'mon! You hit it twice! That is cheating.')
+            else:
+                print('Oh! That was a miss. Try harder next time.')
 
-    if self.filled():
-      print_board(board)
-      print(" ".join(str(coords) for coords in self.coordinates))
-      raise IndexError("A ship already occupies that space.")
+            pc_moves.append(move)
+            
+        ## check if the ships are all destroyed or no more turns
+        if pc_point == 0:
+            print('Congrats, Player! You destroyed all the NPC ships.')
+            break
+        if player_point == 0:
+            print('Game over! Your ships were all destroyed. See you at the bottom!')
+            break
+        if round == 24:
+            if pc_point == player_point:
+                print('No more turns and it\'s a tie.')
+            elif pc_point > player_point:
+                print('No more turns and you LOST.')
+            else: 
+                print('No more turns and you WON. Congrats!')
+        
+        ## change player after each turn
+        if turn == 'Player':
+            turn = 'Computer'
+        else:
+            turn = 'Player'
+            
+        print(f'\n\033[1mPlayer\'s point:\033[0m {player_point} | \033[1mComputer\'s point:\033[0m {pc_point}')
+        
+        round += 1
+    
+    restart = input('Do you want to play again? (Y/N)')
+    if restart == 'Y':
+        game()
     else:
-      self.fillBoard()
-  
-  def filled(self):
-    for coords in self.coordinates:
-      if board[coords['row']][coords['col']] == 1:
-        return True
-    return False
-  
-  def fillBoard(self):
-    for coords in self.coordinates:
-      board[coords['row']][coords['col']] = 1
+        print('Good bye! See you next time.')
 
-  def contains(self, location):
-    for coords in self.coordinates:
-      if coords == location:
-        return True
-    return False
-  
-  def destroyed(self):
-    for coords in self.coordinates:
-      if board_display[coords['row']][coords['col']] == 'O':
-        return False
-      elif board_display[coords['row']][coords['col']] == '*':
-        raise RuntimeError("Board display inaccurate")
-    return True
+        
+print("""
+ ******       **     ********** ********** **       ********  ******** **      ** ** ******* 
+/*////**     ****   /////**/// /////**/// /**      /**/////  **////// /**     /**/**/**////**
+/*   /**    **//**      /**        /**    /**      /**      /**       /**     /**/**/**   /**
+/******    **  //**     /**        /**    /**      /******* /*********/**********/**/******* 
+/*//// ** **********    /**        /**    /**      /**////  ////////**/**//////**/**/**////  
+/*    /**/**//////**    /**        /**    /**      /**             /**/**     /**/**/**      
+/******* /**     /**    /**        /**    /********/******** ******** /**     /**/**/**      
+///////  //      //     //         //     //////// //////// ////////  //      // // //       
+ _                                                                                 _            
+| |__  _   _ _ __ ___   __ _ _ __   __   _____      ___ ___  _ __ ___  _ __  _   _| |_ ___ _ __ 
+| '_ \| | | | '_ ` _ \ / _` | '_ \  \ \ / / __|    / __/ _ \| '_ ` _ \| '_ \| | | | __/ _ \ '__|
+| | | | |_| | | | | | | (_| | | | |  \ V /\__ \_  | (_| (_) | | | | | | |_) | |_| | ||  __/ |   
+|_| |_|\__,_|_| |_| |_|\__,_|_| |_|   \_/ |___(_)  \___\___/|_| |_| |_| .__/ \__,_|\__\___|_|   
+                                                                      |_|                                                                           
+      """
+)
+game()
 
-  
-#Settings Variables
-row_size = 9 #number of rows
-col_size = 9 #number of columns
-num_ships = 4
-max_ship_size = 5
-min_ship_size = 2
-num_turns = 40
-
-#Create lists
-ship_list = []
-
-board = [[0] * col_size for x in range(row_size)]
-
-board_display = [["O"] * col_size for x in range(row_size)]
-
-#Functions
-def print_board(board_array):
-  print("\n  " + " ".join(str(x) for x in range(1, col_size + 1)))
-  for r in range(row_size):
-    print(str(r + 1) + " " + " ".join(str(c) for c in board_array[r]))
-  print()
-
-def search_locations(size, orientation):
-  locations = []
-
-  if orientation != 'horizontal' and orientation != 'vertical':
-    raise ValueError("Orientation must have a value of either 'horizontal' or 'vertical'.")
-
-  if orientation == 'horizontal':
-    if size <= col_size:
-      for r in range(row_size):
-        for c in range(col_size - size + 1):
-          if 1 not in board[r][c:c+size]:
-            locations.append({'row': r, 'col': c})
-  elif orientation == 'vertical':
-    if size <= row_size:
-      for c in range(col_size):
-        for r in range(row_size - size + 1):
-          if 1 not in [board[i][c] for i in range(r, r+size)]:
-            locations.append({'row': r, 'col': c})
-
-  if not locations:
-    return 'None'
-  else:
-    return locations
-
-def random_location():
-  size = randint(min_ship_size, max_ship_size)
-  orientation = 'horizontal' if randint(0, 1) == 0 else 'vertical'
-
-  locations = search_locations(size, orientation)
-  if locations == 'None':
-    return 'None'
-  else:
-    return {'location': locations[randint(0, len(locations) - 1)], 'size': size,\
-     'orientation': orientation}
-
-def get_row():
-  while True:
-    try:
-      guess = int(input("Row Guess: "))
-      if guess in range(1, row_size + 1):
-        return guess - 1
-      else:
-        print("\nOops, that's not even in the ocean.")
-    except ValueError:
-      print("\nPlease enter a number")
-
-def get_col():
-  while True:
-    try:
-      guess = int(input("Column Guess: "))
-      if guess in range(1, col_size + 1):
-        return guess - 1
-      else:
-        print("\nOops, that's not even in the ocean.")
-    except ValueError:
-      print("\nPlease enter a number")
-
-# Create the ships
-
-temp = 0
-while temp < num_ships:
-  ship_info = random_location()
-  if ship_info == 'None':
-    continue
-  else:
-    ship_list.append(Ship(ship_info['size'], ship_info['orientation'], ship_info['location']))
-    temp += 1
-del temp
-
-# Play Game
-os.system('clear')
-print_board(board_display)
-
-for turn in range(num_turns):
-  print("Turn:", turn + 1, "of", num_turns)
-  print("Ships left:", len(ship_list))
-  print()
-  
-  guess_coords = {}
-  while True:
-    guess_coords['row'] = get_row()
-    guess_coords['col'] = get_col()
-    if board_display[guess_coords['row']][guess_coords['col']] == 'X' or \
-     board_display[guess_coords['row']][guess_coords['col']] == '*':
-      print("\nYou guessed that one already.")
-    else:
-      break
-
-  os.system('clear')
-
-  ship_hit = False
-  for ship in ship_list:
-    if ship.contains(guess_coords):
-      print("Hit!")
-      ship_hit = True
-      board_display[guess_coords['row']][guess_coords['col']] = 'X'
-      if ship.destroyed():
-        print("Ship Destroyed!")
-        ship_list.remove(ship)
-      break
-  if not ship_hit:
-    board_display[guess_coords['row']][guess_coords['col']] = '*'
-    print("You missed!")
-
-  print_board(board_display)
-  
-  if not ship_list:
-    break
-
-# End Game
-if ship_list:
-  print("You lose!")
-else:
-  print("All the ships are sunk. You win!")
