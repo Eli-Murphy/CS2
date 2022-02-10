75c93cbfa041dde7e4923c0fc013cdf83b932a14
ILikeBrunch <75328211+ILikeBrunch@users.noreply.github.com>
2022-02-10 10:44:02 -0500

    Update StackOverflow 2-7-22 1049.py

--- Labs/StackOverflow 2-7-22 1049.py
+++ Labs/StackOverflow 2-7-22 1049.py
@@ -3,8 +3,6 @@ from zipfile import ZipFile
 from io import TextIOWrapper
 import csv
 
-with ZipFile(r'C:\Users\emurphy24\Downloads\test.zip') as zf:
-    with zf.open("test.csv", "r") as csvFile:
+with ZipFile('C:\Users\emurphy24\Downloads\test.zip') as zf:
+    with zf.open("name-of-csv-in-zip.csv", "r") as csvFile:
         file = csv.reader(TextIOWrapper(csvFile, 'utf-8'))
-        for row in file:
-            print(row)
